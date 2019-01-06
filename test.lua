8f9cf353a6c09bc3d404fc5a642bec65c0d295e6
Gregory Meyer <gregjm@umich.edu>
2019-01-06 13:41:20 -0500

    update test script

--- test.lua
+++ test.lua
@@ -3,12 +3,16 @@ local larr = require "liblarr"
 local num_elements = 1 << 20
 local array = larr.Vec.with_capacity('integer', num_elements)
 
+local to_push = {}
+for i = 1, num_elements do
+	to_push[i] = i
+end
+
+local iter, invariant, init = ipairs(to_push)
+
 print('pushing ' .. num_elements .. ' elements to larr.Vec')
 local arr_start = os.clock()
-for i = 1, num_elements do
-	array:push(i)
-end
-local arr_str = tostring(array)
+array:append(iter, invariant, init)
 local arr_end = os.clock()
 
 print(arr_end - arr_start, 'seconds elapsed')
@@ -20,23 +24,6 @@ local tbl_start = os.clock()
 for i = 1, num_elements do
 	tbl[i] = i
 end
-
-local tbl_str = {'['}
-
-local first = true
-for i, v in ipairs(tbl) do
-	if not first then
-        tbl_str[#tbl_str + 1] = ', '
-	else
-		first = false
-	end
-
-    tbl_str[#tbl_str + 1] = tostring(v)
-end
-
-tbl_str[#tbl_str + 1] = ']\n'
-
-tbl_str = table.concat(tbl_str)
 local tbl_end = os.clock()
 
 print(tbl_end - tbl_start, 'seconds elapsed')
