f069fa3150e162d0e99dfeb70fe21e7420444287
Gregory Meyer <gregjm@umich.edu>
2019-01-05 20:25:35 -0500

    rename prefix to l_ over larr_, add -DLUA_LIB

--- include/larr/larr.h
+++ include/larr/larr.h
@@ -5,37 +5,40 @@
 extern "C" {
 #endif
 
+#define LUA_LIB
 #include <lua.h>
 
-int larr_Array_new(lua_State *L);
+int l_Array_new(lua_State *L);
 
-int larr_Array_with_capacity(lua_State *L);
+int l_Array_with_capacity(lua_State *L);
 
-int larr_Array_meta_gc(lua_State *L);
+int l_Array_meta_gc(lua_State *L);
 
-int larr_Array_capacity(lua_State *L);
+int l_Array_capacity(lua_State *L);
 
-int larr_Array_meta_len(lua_State *L);
+int l_Array_meta_len(lua_State *L);
 
-int larr_Array_is_empty(lua_State *L);
+int l_Array_is_empty(lua_State *L);
 
-int larr_Array_first(lua_State *L);
+int l_Array_first(lua_State *L);
 
-int larr_Array_last(lua_State *L);
+int l_Array_last(lua_State *L);
 
-int larr_Array_meta_index(lua_State *L);
+int l_Array_meta_index(lua_State *L);
 
-int larr_Array_meta_newindex(lua_State *L);
+int l_Array_meta_newindex(lua_State *L);
 
-int larr_Array_push(lua_State *L);
+int l_Array_push(lua_State *L);
 
-int larr_Array_pop(lua_State *L);
+int l_Array_pop(lua_State *L);
 
-int larr_Array_insert(lua_State *L);
+int l_Array_insert(lua_State *L);
 
-int larr_Array_remove(lua_State *L);
+int l_Array_remove(lua_State *L);
 
-int larr_Array_clear(lua_State *L);
+int l_Array_clear(lua_State *L);
+
+int l_Array_meta_tostring(lua_State *L);
 
 int luaopen_liblarr(lua_State *L);
 
